1aade411024c7fc0315ca7b56d02951266ebbac0
tahoecoop <username@users.noreply.github.com>
2015-07-23 13:33:22 -0700

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Benjamin COOPER on 7/23/15.
+//  Copyright (c) 2015 Ben Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Benjamin COOPER on 7/23/15.
+//  Copyright (c) 2015 Ben Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
